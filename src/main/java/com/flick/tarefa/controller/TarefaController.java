332cc62463a1fe3fe6370b1b821122b72424dcd9
DeltaFlick12 <pedrolucasgomesbr1212@gmail.com>
2026-03-22 22:34:17 -0300

    controller criado

---
+++ src/main/java/com/flick/tarefa/controller/TarefaController.java
@@ -0,0 +1,47 @@
+package com.flick.tarefa.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.flick.tarefa.model.Tarefa;
+import com.flick.tarefa.service.TarefaService;
+
+@RestController
+@RequestMapping("/tarefas")
+public class TarefaController {
+
+    @Autowired
+    private TarefaService tarefaService;
+
+    @PostMapping
+    public Tarefa criarTarefa(@RequestBody Tarefa tarefa) {
+        return tarefaService.criarTarefa(tarefa);
+    }
+
+    @GetMapping
+    public List<Tarefa> listarTarefas() {
+        return tarefaService.listarTarefas();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tarefa> buscarTarefaPorId(@PathVariable Long id) {
+        return tarefaService.buscarTarefaPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarTarefa(@PathVariable Long id) {
+        tarefaService.deletarTarefa(id);
+        return ResponseEntity.noContent().build();
+    }
+}
